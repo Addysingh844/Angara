6b93adda2d1d234677336d5a0e3f97305e1e30b2
Addysingh844 <addysingh844@gmail.com>
2024-05-31 19:21:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,15 +13,13 @@ driver = webdriver.Chrome()
 try:
     # Step 1: Visit the Angara.in homepage
     driver.get("https://www.angara.in")
+    time.sleep(8)
 
-    # Check if alert is present, if yes, accept it
-    try:
-        alert = driver.switch_to.alert
-        alert.accept()
-    except NoAlertPresentException:
-        pass
+    # Step 2: To close popup
+    driver.find_element(By.CLASS_NAME, "ecomsend__Modal__CloseText._closeText_4tu8s_174").click()
+    time.sleep(4)
 
-    # Step 2: Search for a product
+    # Step 3: Search for a product
     driver.find_element(By.CLASS_NAME, "px1.mb0.inline-block.lh0.js-slideout-open").click()
     time.sleep(4)
     driver.find_element(By.CLASS_NAME, "searchbox__input.mb0.mt5.no-bg-color").click()
@@ -29,22 +27,22 @@ try:
     search_box.send_keys("24KT (999) Goddess Lakshmi Yellow Gold Coin")
     search_box.send_keys(Keys.RETURN)
 
-    # Step 3: Select the product from the list
+    # Step 4: Select the product from the list
     time.sleep(5)
     product_link = driver.find_element(By.LINK_TEXT, "24KT (999) Goddess Lakshmi Yellow Gold Coin")
     product_link.click()
 
-    # Step 4: Add the product to the cart
+    # Step 5: Add the product to the cart
     time.sleep(5)
     add_to_cart_button = driver.find_element(By.ID, "add-to-card-btn")
     add_to_cart_button.click()
 
-    # Step 5: Proceed to checkout
+    # Step 6: Proceed to checkout
     time.sleep(3)
     checkout_button = driver.find_element(By.ID, "checkout")
     checkout_button.click()
 
-    # Step 6: Fill in information details
+    # Step 7: Fill in information details
     time.sleep(3)
     driver.find_element(By.NAME, "email").send_keys("addysingh844@gmail.com")
     driver.find_element(By.NAME, "firstName").send_keys("Aditya")
@@ -54,13 +52,13 @@ try:
     driver.find_element(By.NAME, "postalCode").send_keys("110019")
     driver.find_element(By.NAME, "phone").send_keys("1122334455")
 
-    # Step 7: Proceed to shipping
+    # Step 8: Proceed to shipping
     time.sleep(5)
 
     Continue_to_shipping = driver.find_element(By.CLASS_NAME, "QT4by._1fragempw.rqC98._1m2hr9gd._1m2hr9ga._7QHNJ.VDIfJ.j6D1f.janiy")
     Continue_to_shipping.click()
 
-    # Step 8: Continue to payment
+    # Step 9: Continue to payment
     time.sleep(8)
 
     Continue_to_payment = driver.find_element(By.CLASS_NAME, "QT4by._1fragempw.rqC98._1m2hr9gd._1m2hr9ga._7QHNJ.VDIfJ.j6D1f.janiy")
@@ -68,7 +66,7 @@ try:
 
     time.sleep(5)
 
-    # Step 9: Page to Pay now
+    # Step 10: Page to Pay now
 
     driver.find_element(By.CLASS_NAME, "QT4by._1fragempw.rqC98._1m2hr9gd._1m2hr9ga._7QHNJ.VDIfJ.j6D1f.janiy").click()
 
